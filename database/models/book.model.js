5d27bfa8683c23bcdf0bf4da46614d9d329c38c8
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-05-05 15:44:07 +0200

    Added jsdocs to readingList

--- database/models/book.model.js
+++ database/models/book.model.js
@@ -5,6 +5,7 @@
  * @type {Object}       Book
  * @type {Object}       Book.title
  * @type {Object}       Book.author
+ *
  * @returns {Object}    Book object representing a database function
  */
 module.exports = (Sequelize, connector) => {
--- database/models/readingList.model.js
+++ database/models/readingList.model.js
@@ -1,3 +1,13 @@
+/**
+ * Creates a model for 'reading_lists' table.
+ * @function
+ * @module book/model
+ * @type {Object}       ReadingList
+ * @type {Object}       ReadingList.name
+ * @type {Object}       ReadingList.type
+ *
+ * @returns {Object}    ReadingList object representing a database function
+ */
 module.exports = (Sequelize, connector) => {
   const ReadingList = connector.define("reading_list", {
     name: {
