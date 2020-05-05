809b30bb0ab3e80a9fb8008068bad2d05d96ccf3
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-05-05 15:42:45 +0200

    New jsdocs for book models and db.configs

--- database/configs/db.config.js
+++ database/configs/db.config.js
@@ -1,7 +1,24 @@
 require("dotenv").config();
 
+/**
+ * @const {Object} process.env                Environment variables
+ * @const {string} process.env.DB_HOST        Database host value
+ * @const {string} process.env.DB_USER        Database user value
+ * @const {string} process.env.DB_PASSWORD    Database password value
+ * @const {string} process.env.DB_NAME        Database name value
+ * @const {string} process.env.DB_DIALECT     Database dialect value
+ */
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_DIALECT } = process.env;
 
+/**
+ * @module database/configuration
+ * @type {Object}                             db.config
+ * @type {string}                             db.config.host
+ * @type {string}                             db.config.user
+ * @type {string}                             db.config.password
+ * @type {string}                             db.config.name
+ * @type {string}                             db.config.dialect
+ */
 module.exports = {
   host: DB_HOST,
   user: DB_USER,
--- database/models/book.model.js
+++ database/models/book.model.js
@@ -1,3 +1,12 @@
+/**
+ * Creates a model for 'books' table.
+ * @function
+ * @module book/model
+ * @type {Object}       Book
+ * @type {Object}       Book.title
+ * @type {Object}       Book.author
+ * @returns {Object}    Book object representing a database function
+ */
 module.exports = (Sequelize, connector) => {
   const Book = connector.define("book", {
     title: {
