5eddde253d798c6f17c2fc6d8ed3558fb1a41426
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-05-05 17:24:15 +0200

    new jsdocs

--- database/models/index.js
+++ database/models/index.js
@@ -1,12 +1,39 @@
+/**
+ * @const Sequelize                       Sequelize ORM dependency
+ */
 const Sequelize = require("sequelize");
 
+/**
+ * @const {Object} db.config              Object containing database configurations
+ * @const {string} db.config.host         Database host value
+ * @const {string} db.config.user         Database user value
+ * @const {string} db.config.password     Database password value
+ * @const {string} db.config.name         Database name value
+ * @const {string} db.config.dialect      Database dialect value
+ */
 const { host, user, password, name, dialect } = require("../configs/db.config");
 
+/**
+ * Connects to the database by creating a Sequelize instance.
+ * @instance
+ * @requires module:sequelize
+ *
+ * @param {string}        name
+ * @param {string}        user
+ * @param {string}        password
+ * @param {object.string} options.host
+ * @param {object.string} options.dialect
+ *
+ */
 const connector = new Sequelize(name, user, password, {
   host,
   dialect,
 });
 
+/**
+ * Authenticate the database connection
+ * @return {Promise}
+ */
 connector
   .authenticate()
   .then(() => console.log(`Connection to database is successful`))
@@ -14,6 +41,15 @@ connector
     console.error(`Connection to database was unsuccessful: ${err}`)
   );
 
+/**
+ * Initiates a database object with the connector, models, and associations.
+ * @const db                               Database object
+ * @type {Sequelize.constructor}           db.Sequelize
+ * @type {Sequelize.instance}              db.connector
+ * @type {Sequelize.model}                 db.readingList
+ * @type {Sequelize.model}                 db.book
+ *
+ */
 const db = {};
 
 db.Sequelize = Sequelize;
@@ -23,4 +59,7 @@ db.book = require("./book.model")(Sequelize, connector);
 
 db.book.belongsTo(db.readingList);
 
+/**
+ * @module database/object
+ */
 module.exports = db;
