10dc2d40786024c3d400fa2f6bd01240c76ddbba
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-05-05 18:57:05 +0200

    Added documentation to server.js

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 require("dotenv").config();
-
+/**
+ * Fires up the server to given port
+ * @requires module:app.js
+ *
+ * @const server                                Express app
+ * @const port                                  Port on which server will run on
+ * @method express.listen(string,callback)      Listens for connections on the specified host and port.
+ */
 const server = require("./app");
 
 const port = process.env.PORT;
