01139d73d868d0e3bc81667e6703b06897fc3aad
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-05-06 19:23:15 +0200

    Added new tests

--- helpers/crudOperations/__tests__/findAll.helper.test.js
+++
@@ -1,8 +0,0 @@
-const findAll = require("../findAll.helper");
-
-const { mockResponse } = require("../../../utils/interceptor");
-
-const res = mockResponse;
-test("", () => {
-  console.log(findAll);
-});
