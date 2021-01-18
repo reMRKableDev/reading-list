024ea88c70e51f8eb479ab3dea74c787c84636f5
Malcolm Kente <malcolm.kente@remrkabledev.com>
2021-01-18 23:46:30 +0100

    :art: removing jsdoc on app.js and server.js

--- app.js
+++ app.js
@@ -1,22 +1,8 @@
-/**
- * @const express                               Imports express dependency
- * @requires module:express
- *
- * @const morgan                                Imports morgan dependency
- * @requires module:morgan
- */
 const express = require("express");
 const morgan = require("morgan");
 
-/**
- * @const app                                   Express app
- */
 const app = express();
 
-/**
- * Mounts specified middleware function(s) at the specified path
- * @method app.use(*)
- */
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -24,7 +10,4 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/readList", require("./routes/readingList.routes"));
 app.use("/api/books", require("./routes/books.routes"));
 
-/**
- * @module app
- */
 module.exports = app;
--- server.js
+++ server.js
@@ -1,24 +1,8 @@
 require("dotenv").config();
-/**
- * Fires up the server to given port
- * @const server                                Express app
- * @requires module:app.js
- *
- * @const port                                  Port on which server will run on
- *
- * @const connector                             Retrieves database connector
- * @type {Sequelize.model}
- *
- * @method connector.sync()                     Sync all defined models to the database.
- * @returns {Promise}
- *
- * @method express.listen(string,callback)      Listens for connections on the specified host and port.
- */
 
 const server = require("./app");
 
 const port = process.env.PORT;
-
 const { connector } = require("./database/models");
 
 connector
